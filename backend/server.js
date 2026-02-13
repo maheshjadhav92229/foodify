78175aea4eab3327dffd181ff3f8f26634076a70
mahesh <maheshjadhav92229@gmail.com>
2026-02-14 01:30:32 +0530

    Fixed dotenv order and PORT for Render

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
@@ -7,7 +8,7 @@ import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 
 
-
+dotenv.config()
 
 //app config
 const app = express() 
@@ -29,8 +30,7 @@ app.use("/api/cart",cartRouter);
 app.use("/api/order",orderRouter)
 
 
-import dotenv from "dotenv"
-dotenv.config()
+
 
 
 
